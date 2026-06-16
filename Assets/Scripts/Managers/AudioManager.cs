7033e20af849a61a8ff72030e41be3ef91ad00d8
CUR4 <manucura11@gmail.com>
2026-06-16 17:34:15 -0300

    Audio manager on menu, move player Asource

---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioManager Instance;
+
+    [SerializeField] private AudioSource musicSource;
+    [SerializeField] private AudioSource sfxSource;
+
+    private float musicVolume;
+    private float sfxVolume;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    } 
+
+    public void PlayMusic(AudioClip clip)
+    {
+        musicSource.clip = clip;
+        musicSource.Play();
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        sfxSource.PlayOneShot(clip);
+    }
+}
